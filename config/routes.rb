d02dec5ffa8f0ef4dd7ba44275e7bf7407d2c12f
j19015 <koki.weben.main@gmail.com>
2024-02-20 15:42:02 +0000

    add routes.rbの記述を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks, only: [:index, :new, :create, :destroy]
 end
